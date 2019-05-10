70de060f53ce7ce4b78e2fa27e44ce9f20fe2a7c
wzdjs <958078744@qq.com>
2019-05-10 18:00:57 +0800

    combineReducers方法

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,15 +1,20 @@
+import { combineReducers } from 'redux'
 import todoRucer from '@/views/Home/Cinemas/store/reducer.js'
 import movieReducer from '@/views/Home/Movies/store/reducer.js'
 
-const defaultState = {}
+// const defaultState = {}
 
- export default (state = defaultState, action) => {
-    return {
-     /**
-     *@return { key : value} key 模块  value模块reducer函数的执行
-     *
-     */
-      TODO: todoRucer(state.TODO,action),
-      MOVIE: movieReducer(state.MOVIE,action)
-    }
- }
+//  export default (state = defaultState, action) => {
+//     return {
+//      /**
+//      *@return { key : value} key 模块  value模块reducer函数的执行
+//      *
+//      */
+//       TODO: todoRucer(state.TODO,action),
+//       MOVIE: movieReducer(state.MOVIE,action)
+//     }
+//  }
+export default combineReducers({
+   TODO: todoRucer,
+   MOVIE:movieReducer
+})
