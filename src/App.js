e34fb609f73e38b763dddab4d89ad0bdfb7aa885
wzdjs <958078744@qq.com>
2019-05-13 15:05:44 +0800

    高阶组件的应用

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Home from './views/Home'
 import Detail from './views/Detail'
 import Login from './views/Login'
 import './index.less'
+import hoc from '@/common/Hoc'
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path="/Detail" component={ Detail }></Route>
+        <Route path="/Detail" component={ hoc(Detail) }></Route>
         <Route path="/Login" component={ Login }></Route>
         <Route path="/" component={ Home }></Route>
       </Switch>
