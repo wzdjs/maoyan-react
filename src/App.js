0c76ff1ae6fb95790cb2e021fc1ee8fe62648424
wzdjs <958078744@qq.com>
2019-05-13 16:45:19 +0800

    页面的路由拦截

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Detail from './views/Detail'
 import Login from './views/Login'
 import './index.less'
 import hoc from '@/common/Hoc'
+import RouterTest from './common/RouterTest'
 
 class App extends React.Component {
   render() {
@@ -12,7 +13,7 @@ class App extends React.Component {
       <Switch>
         <Route path="/Detail" component={ hoc(Detail) }></Route>
         <Route path="/Login" component={ Login }></Route>
-        <Route path="/" component={ Home }></Route>
+        <Route path="/" component={ RouterTest(Home) }></Route>
       </Switch>
     )
   }
